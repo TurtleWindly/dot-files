847931a3f5fc107bd0c0fa2d14cdb3177fbb032e
Minh <quachquangminh@gmail.com>
2022-04-20 17:50:25 +0700

    Add run command

---
+++ rofi/launchers/run.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+# Available Styles
+# >> Created and tested on : rofi 1.6.0-1
+#
+# style_1     style_2     style_3     style_4     style_5     style_6
+# style_7     style_8     style_9     style_10    style_11    style_12
+
+theme="style"
+dir="$HOME/.config/rofi/launchers"
+
+# dark
+ALPHA="#ff00ff00"
+BG="#000000aa"
+FG="#DDDDDDff"
+SELECT="#afffaaff"
+
+# light
+#ALPHA="#00000000"
+#BG="#FFFFFFff"
+#FG="#000000ff"
+#SELECT="#f3f3f3ff"
+
+# accent colors
+# COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
+# 		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
+# ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
+ACCENT="#FA8334"
+
+# overwrite colors file
+cat > $dir/colors.rasi <<- EOF
+	/* colors */
+
+	* {
+	  al:  $ALPHA;
+	  bg:  $BG;
+	  se:  $SELECT;
+	  fg:  $FG;
+	  ac:  $ACCENT;
+	}
+EOF
+
+rofi -no-lazy-grab -show run -modi run -theme $dir/"$theme"
