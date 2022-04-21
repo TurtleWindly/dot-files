f4752d926a986ebc59fa3f705d7322dbddcbc442
Minh <quachquangminh@gmail.com>
2022-04-21 13:18:16 +0700

    fast reload

---
+++ reload.sh
@@ -0,0 +1,13 @@
+rm -r ./i3/*
+cp -r ~/.config/i3 .
+rm -r ./i3status/*
+cp -r ~/.config/i3status .
+rm -r ./rofi/*
+cp -r ~/.config/rofi .
+rm -r ./polybar/*
+cp -r ~/.config/polybar .
+rm -r ./picom/*
+cp -r ~/.config/picom .
+rm -r ./terminator/*
+cp -r ~/.config/terminator .
+cp -r ~/.config/nvim .
